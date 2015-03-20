dabe52fa7ed401ed794b20fd125fcda1d477d5f1
Andrii Vasylchenko <andrey.vasylchenko@gmail.com>
2015-03-20 23:01:46 +0100

    initial application project commit

---
+++ apps/cidemo/android/native/src/com/cidemo/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.cidemo;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
---
+++ apps/cidemo/iphone/native/cidemoCidemoIphone.xcarchive/Products/Applications/cidemo.app/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":4124528607,"date":1426888444018,"machine":"Andriis-MBP"};
+/* Date: Fri Mar 20 22:54:04 CET 2015 */
